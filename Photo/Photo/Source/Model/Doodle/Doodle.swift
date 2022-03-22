d9aa528646c408ba51fa64ba1e31299d2d6ec412
shingha <shingha1124@gmail.com>
2022-03-22 16:57:35 +0900

    [feature] json파싱을 위한 작업 진행

---
+++ Photo/Photo/Source/Model/Doodle/Doodle.swift
@@ -0,0 +1,15 @@
+//
+//  Doodle.swift
+//  Photo
+//
+//  Created by seongha shin on 2022/03/22.
+//
+
+import Foundation
+
+struct Doodle: Decodable {
+    let title: String
+    let image: URL
+    let date: String
+}
+
---
+++ Photo/Photo/Source/Model/Doodle/Doodles.swift
@@ -0,0 +1,12 @@
+//
+//  Doodles.swift
+//  Photo
+//
+//  Created by seongha shin on 2022/03/22.
+//
+
+import Foundation
+
+struct Doodles: Decodable {
+    let data: [Doodle]
+}
